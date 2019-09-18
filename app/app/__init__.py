f71163c52f1e993ea39f17d1c2ac11a5364cf9c5
chintan <chintanjoshi2012@gmail.com>
2019-09-18 11:02:25 -0700

    reset

--- app/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import Config
-from flask_restful import Resource, Api
-
-app=Flask(__name__)
-app.config.from_object(Config)
-api=Api(app)
-from app import cleo
