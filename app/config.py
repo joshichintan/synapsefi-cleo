79a3fd7316cd7e7c912bf8c06d9846a695804c40
chintan <chintanjoshi2012@gmail.com>
2019-09-18 17:00:12 -0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
 
-    MONGO_URI = "mongodb://admin:password@mongodb/mercury"
+    MONGO_URI = ""
     CLIENT_ID="client_id_GFjqYduxbBIlTNZ3DVPgco85XzWsUifML4Rr19yC"
     CLIENT_SECRET="client_secret_E1lsFwicjX0HpCkP6AB4z5OtuIvyUoqV0a7JmK28"
     FINGERPRINT="123456"
